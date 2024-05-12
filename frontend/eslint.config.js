022e1e19245c6d3f32757fc03b019f75b68b038b
thelicato <thelicato@users.noreply.github.com>
2024-05-12 13:30:15 +0200

    feat: add initial frontend files

---
+++ frontend/eslint.config.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-empty-interface': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
+});
