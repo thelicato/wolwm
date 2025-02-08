b50654e7ff74f863c9b1a4bf90c8c50e6d29304f
thelicato <thelicato@users.noreply.github.com>
2025-02-08 14:45:36 +0100

    feat: initial Events table

--- frontend/src/views/Events.tsx
+++ frontend/src/views/Events.tsx
@@ -1,14 +1,157 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import {
+  ColumnDef,
+  PaginationState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 import toast from 'react-hot-toast';
 import { MdError } from 'react-icons/md';
+import { IoChevronDown, IoChevronUp } from 'react-icons/io5';
 import { IEvent } from '@/types';
 import { RESTManagerInstance } from '@/utils/rest';
 import { sleep } from '@/utils/helpers';
 import { Loading } from '@/components';
 
+const EventsTable = ({ data, columns }: { data: IEvent[]; columns: ColumnDef<IEvent>[] }) => {
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+
+  const table = useReactTable({
+    columns,
+    data,
+    debugTable: true,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onPaginationChange: setPagination,
+    //no need to pass pageCount or rowCount with client-side pagination as it is calculated automatically
+    state: {
+      pagination,
+    },
+    // autoResetPageIndex: false, // turn off page index reset when sorting or filtering
+  });
+
+  return (
+    <div className='w-full'>
+      <table className='w-full rounded-t-md overflow-hidden border-collapse'>
+        <thead className='bg-gray-800 text-white h-10'>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header, index) => {
+                const isLast = index === headerGroup.headers.length -1
+                return (
+                  <th key={header.id} colSpan={header.colSpan} className={isLast ? '' : 'box-border border-r border-gray-400'}>
+                    <div
+                      {...{
+                        className: header.column.getCanSort() ? 'flex justify-center items-center gap-2 cursor-pointer select-none' : '',
+                        onClick: header.column.getToggleSortingHandler(),
+                      }}
+                    >
+                      {flexRender(header.column.columnDef.header, header.getContext())}
+                      {{
+                        asc: <IoChevronUp className='inline' />,
+                        desc: <IoChevronDown className='inline' />,
+                      }[header.column.getIsSorted() as string] ?? null}
+                    </div>
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row, index) => {
+            const rowBg = index % 2 === 0 ? 'bg-white' : 'bg-gray-100';
+
+            return (
+              <tr key={row.id} className={rowBg}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td key={cell.id} className='text-center py-2 box-border border border-gray-400'>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className='flex items-center gap-2'>
+        <button
+          className='border rounded p-1'
+          onClick={() => table.firstPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<<'}
+        </button>
+        <button
+          className='border rounded p-1'
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<'}
+        </button>
+        <button
+          className='border rounded p-1'
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>'}
+        </button>
+        <button
+          className='border rounded p-1'
+          onClick={() => table.lastPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>>'}
+        </button>
+        <span className='flex items-center gap-1'>
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of {table.getPageCount().toLocaleString()}
+          </strong>
+        </span>
+      </div>
+    </div>
+  );
+};
 export const Events = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [events, setEvents] = useState<IEvent[]>([]);
+
+  const columns = useMemo<ColumnDef<IEvent>[]>(
+    () => [
+      {
+        accessorFn: (row) => row.eventType,
+        id: 'eventType',
+        cell: (e) => e.getValue(),
+        header: () => <span>Type</span>,
+        footer: (props) => props.column.id,
+      },
+      {
+        accessorKey: 'eventData',
+        header: () => 'Data',
+        footer: (props) => props.column.id,
+      },
+      {
+        accessorFn: (row) => row.timestamp.slice(0,-7),
+        id: 'timestamp',
+        cell: (e) => e.getValue(),
+        header: () => <span>Timestamp</span>,
+        footer: (props) => props.column.id,
+      },
+    ],
+    [],
+  );
 
   const loadEvents = async () => {
     try {
@@ -36,7 +179,7 @@ export const Events = () => {
         <div className='p-4'>
           <h1 className='font-bold text-4xl mb-4'>Events</h1>
           <div className='w-full flex flex-col justify-center items-center'>
-            <MdError size={100} className='dark:text-white'/>
+            <MdError size={100} className='dark:text-white' />
             <h2 className='text-2xl font-semibold'>No Events</h2>
           </div>
         </div>
@@ -48,18 +191,7 @@ export const Events = () => {
     <>
       <div className='p-4'>
         <h1 className='font-bold text-4xl mb-4'>Events</h1>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-          {events.map((event: IEvent, key: number) => {
-            return (
-              <div className='card p-4' key={key}>
-                <h2 className='text-2xl font-bold mb-2'>{event.eventType}</h2>
-                <p>
-                  <b></b> {event.eventData}
-                </p>
-              </div>
-            );
-          })}
-        </div>
+        <EventsTable columns={columns} data={events} />
       </div>
     </>
   );
