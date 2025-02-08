c457ca3b539c155c9e611551c5c9b7fad2818a42
thelicato <thelicato@users.noreply.github.com>
2025-02-08 15:32:59 +0100

    feat: allow event filtering

--- frontend/src/views/Events.tsx
+++ frontend/src/views/Events.tsx
@@ -9,22 +9,32 @@ import {
   getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table';
+import Fuse from 'fuse.js';
 import toast from 'react-hot-toast';
 import { MdError } from 'react-icons/md';
 import { IoChevronDown, IoChevronUp } from 'react-icons/io5';
-import { HiChevronLeft, HiChevronRight, HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi2";
+import {
+  HiChevronLeft,
+  HiChevronRight,
+  HiChevronDoubleLeft,
+  HiChevronDoubleRight,
+} from 'react-icons/hi2';
 import { IEvent, EventType } from '@/types';
 import { RESTManagerInstance } from '@/utils/rest';
 import { sleep } from '@/utils/helpers';
 import { Loading } from '@/components';
 
-const EVENT_TYPE_CLASSNAMES = "rounded-md text-white p-[4px]"
+const EVENT_TYPE_CLASSNAMES = 'rounded-md text-white p-[4px] text-nowrap';
 
-const EVENT_TYPE_MAPPINGS: {[key in EventType]: JSX.Element} = {
-  [EventType.DEVICE_ADDED]: <span className={`${EVENT_TYPE_CLASSNAMES} bg-teal-500`}>DEVICE ADDED</span>,
-  [EventType.DEVICE_REMOVED]: <span className={`${EVENT_TYPE_CLASSNAMES} bg-rose-500`}>DEVICE REMOVED</span>,
-  [EventType.WAKE]: <span className={`${EVENT_TYPE_CLASSNAMES} bg-indigo-500`}>WAKE REQUEST</span>
-}
+const EVENT_TYPE_MAPPINGS: { [key in EventType]: JSX.Element } = {
+  [EventType.DEVICE_ADDED]: (
+    <span className={`${EVENT_TYPE_CLASSNAMES} bg-teal-500`}>DEVICE ADDED</span>
+  ),
+  [EventType.DEVICE_REMOVED]: (
+    <span className={`${EVENT_TYPE_CLASSNAMES} bg-rose-500`}>DEVICE REMOVED</span>
+  ),
+  [EventType.WAKE]: <span className={`${EVENT_TYPE_CLASSNAMES} bg-indigo-500`}>WAKE REQUEST</span>,
+};
 
 const EventsTable = ({ data, columns }: { data: IEvent[]; columns: ColumnDef<IEvent>[] }) => {
   const [pagination, setPagination] = useState<PaginationState>({
@@ -148,6 +158,7 @@ const EventsTable = ({ data, columns }: { data: IEvent[]; columns: ColumnDef<IEv
 export const Events = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [events, setEvents] = useState<IEvent[]>([]);
+  const [visibleEvents, setVisibleEvents] = useState<IEvent[]>([])
 
   const columns = useMemo<ColumnDef<IEvent>[]>(
     () => [
@@ -174,10 +185,17 @@ export const Events = () => {
     [],
   );
 
+  const fuse = new Fuse(events, {
+    includeScore: false,
+    threshold: 0.3,
+    keys: ['eventType', 'eventData']
+  })
+
   const loadEvents = async () => {
     try {
       const res = await RESTManagerInstance.getEvents();
       setEvents(res.data.events);
+      setVisibleEvents(res.data.events);
       await sleep(500);
       setIsLoading(false);
       toast.success('Events loaded');
@@ -190,29 +208,52 @@ export const Events = () => {
     loadEvents();
   }, []);
 
-  if (isLoading) {
-    return <Loading blur />;
+  const searchEvents = (input: string) => {
+    if (input.trim().length === 0) {
+      setVisibleEvents(events)
+    } else {
+      const res = fuse.search(input);
+      setVisibleEvents(res.map(r => r.item))
+    }
   }
 
-  if (events.length === 0) {
-    return (
-      <>
-        <div className='p-4'>
-          <h1 className='font-bold text-4xl mb-4'>Events</h1>
-          <div className='w-full flex flex-col justify-center items-center'>
-            <MdError size={100} className='dark:text-white' />
-            <h2 className='text-2xl font-semibold'>No Events</h2>
-          </div>
-        </div>
-      </>
-    );
+  if (isLoading) {
+    return <Loading blur />;
   }
 
   return (
     <>
       <div className='p-4'>
         <h1 className='font-bold text-4xl mb-4'>Events</h1>
-        <EventsTable columns={columns} data={events} />
+        <div className='w-full md:max-w-sm min-w-[200px] mb-4'>
+          <div className='relative flex items-center'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              fill='currentColor'
+              className='absolute w-5 h-5 top-2.5 left-2.5 text-slate-600'
+            >
+              <path
+                fill-rule='evenodd'
+                d='M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z'
+                clip-rule='evenodd'
+              />
+            </svg>
+
+            <input
+              className='w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow'
+              placeholder='Search event'
+              onChange={e => searchEvents(e.currentTarget.value)}
+            />
+          </div>
+        </div>
+        {visibleEvents.length > 0 ? (<EventsTable columns={columns} data={visibleEvents} />) : (
+                    <div className='w-full flex flex-col justify-center items-center'>
+                    <MdError size={100} className='dark:text-white' />
+                    <h2 className='text-2xl font-semibold'>No Events</h2>
+                  </div>
+        )}
+        
       </div>
     </>
   );
